f81144363b62d82e381581c8fabec9b7625b2691
chinnu rajan <ch.rajan@outlook.com>
2025-01-30 22:31:32 +0000

    Add project files.

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,71 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> CreateCouponsAsync(CouponDto coupon)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.POST,
+                Data=coupon,
+                Url = SD.CouponAPIBase + "/api/Coupon" 
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteCouponsAsync(int Id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.DELETE,
+                Url = SD.CouponAPIBase + "/api/Coupon/" + Id
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponByAsc(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/Coupon/GetByCode/" + couponCode
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponByIdAsync(int Id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/Coupon/"+Id
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllCouponsAsyc()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+                {
+                     ApiType = Utility.SD.ApiType.GET,
+                      Url=SD.CouponAPIBase+ "/api/Coupon"
+            }); 
+        } 
+
+        public async Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = Utility.SD.ApiType.PUT,
+                Data = couponDto,
+                Url = SD.CouponAPIBase + "/api/Coupon"
+            });
+        } 
+    }
+}
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponByAsc(string couponCode);
+        Task<ResponseDto?> GetAllCouponsAsyc();
+        Task<ResponseDto?> GetCouponByIdAsync(int Id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDto coupon);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponsAsync(int Id);
+    }
+}
